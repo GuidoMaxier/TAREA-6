994036ded861baa18efbb072166fb1681722cf84
Hernan Casasola <67485423+GuidoMaxier@users.noreply.github.com>
2024-07-07 19:24:16 -0300

    feat: app.java completo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        SistemaJuego sistemaJuego = new SistemaJuego();
+        Thread hiloEmparejamiento = new Thread(sistemaJuego);
+        hiloEmparejamiento.start();
+
+        int cantJugadores = 20;
+        GeneradorJugador generadorJugador = new GeneradorJugador(sistemaJuego, cantJugadores);
+
+        Thread hiloGenerador = new Thread(generadorJugador);
+        hiloGenerador.start();
+
+        try {
+            hiloGenerador.join();
+            hiloEmparejamiento.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("El programa ha finalizado");
+
     }
 }
