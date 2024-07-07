386011dcd96a900eaffae8f611b4d11793c4e2fb
Hernan Casasola <67485423+GuidoMaxier@users.noreply.github.com>
2024-07-07 12:04:24 -0300

    feat (run): Clase Partida

--- src/Partida.java
+++ src/Partida.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Partida implements Runnable {
     
     private final Jugador jugador1;
@@ -14,17 +16,40 @@ public class Partida implements Runnable {
     public void run() {
         //  1. Aumentar la cantidad de partidas activas en el sistema (método a especificar en 
         // esta clase) 
+        sistemaJuego.aumentarPartidasActivas();
+
         // 2. Indicar que empezó una partida entre los jugadores correspondientes.
+        System.out.println("Empezó una partida entre " + jugador1.getId() + " y " + jugador2.getId());
         // 3. Simular la partida con una espera de 3 segundos.
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         // 4. Generar un puntaje aleatorio para cada uno con un valor aleatorio entre 0 y 5.
+        Random random = new Random();
+        int puntosJugador1 = random.nextInt(6);
+        int puntosJugador2 = random.nextInt(6);
+
         // 5. Calculamos quién fue el ganador según los resultados.
+        String resultado;
+        if (puntosJugador1 > puntosJugador2) {
+            resultado = "Jugador [" + jugador1.getId() + "] WINS!"; 
+        } else if (puntosJugador1 < puntosJugador2) {
+            resultado = "Jugador [" + jugador2.getId() + "] WINS!"; 
+        } else {
+            resultado = "Empate";
+        }
+
         // 6. Mostramos el resultado final de la partida con el siguiente formato:
-        // a. Resultado de la partida: Jugador [id] + ( puntos ) + vs Jugador [id] + ( 
+           // a. Resultado de la partida: Jugador [id] + ( puntos ) + vs Jugador [id] + ( 
         // puntos ) – Resultado del cálculo en (5)
-        // 7. Disminuir la cantidad de partidas activas en el sistema.
+        System.out.println("Resultado de la partida: Jugador [" + jugador1.getId() + "] (" + puntosJugador1 + ") vs Jugador [" + jugador2.getId() + "] (" + puntosJugador2 + ") - " + resultado);
+        
+        // 7. Disminuir la cantidad de partidas activas en el sistema.¨
+        sistemaJuego.disminuirPartidasActivas();
         
     }
-
-
 
 }
