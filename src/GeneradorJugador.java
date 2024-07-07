092c4f9ed2a3d98e55924b574140d85dc50396f8
Hernan Casasola <67485423+GuidoMaxier@users.noreply.github.com>
2024-07-07 18:28:56 -0300

    feat: Clase GeneradorJugador

---
+++ src/GeneradorJugador.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+public class GeneradorJugador implements Runnable {
+    private final SistemaJuego sistemaJuego;
+    private final int jugadoresTotales;
+
+    public GeneradorJugador(SistemaJuego sistemaJuego, int jugadoresTotales) {
+        this.sistemaJuego = sistemaJuego;
+        this.jugadoresTotales = jugadoresTotales;
+    }
+
+    @Override
+    public void run(){
+        Random random = new Random();
+        for (int i = 1; i <= jugadoresTotales; i++) {
+
+            try {
+                Thread.sleep(random.nextInt(5000));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            int ranking = random.nextInt(23001) + 1000;
+            Jugador jugador = new Jugador(i, ranking);
+            sistemaJuego.agregarJugador(jugador);
+
+            //El Jugador [id] (Ranking: [ranking]) se unió a la cola de espera.
+            System.out.println("El Jugador ["+ jugador.getId() +"] (Ranking: [" + jugador.getRanking() +"]) se unió a la cola de espera.");
+
+        }
+        sistemaJuego.setGenerandoJugadores(false);
+        System.out.println("Se terminaron de generar los " + jugadoresTotales + " jugadores indicados.");
+    }
+    
+}
